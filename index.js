74523663844c6bfb7269833b31a990433e14278e
Kamal Sagderov <sagderovkamal.it@gmail.com>
2024-12-11 01:08:20 +0300

    add app page

---
+++ index.js
@@ -0,0 +1,12 @@
+let counter = 0;
+
+console.log(counter);
+
+counter = counter + 1;  // 0 + 1
+console.log(counter);
+
+counter = counter + 1; // 1 + 1
+console.log(counter);
+
+counter = counter + 1;
+console.log(counter);
